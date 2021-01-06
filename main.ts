84b4fb148a0962d42bd840bbf7896ca44b645cf6
mihirrajkumar <77078237+mihirrajkumar@users.noreply.github.com>
2021-01-06 15:36:27 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    music.playMelody("E B C5 A B G A F ", 500)
+    music.playMelody("F B C5 A B G A F ", 500)
+    music.playMelody("E B C5 A B G A F ", 500)
+    music.ringTone(392)
+})
